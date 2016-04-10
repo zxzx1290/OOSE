259ef4589db1daf029935c11bbd5d42cdb361e4c
zxzx1290 <zxl1l2l3@yahoo.com.tw>
2016-04-10 15:50:47 +0800

    Create Strategy.java

---
+++ Strategy.java
@@ -0,0 +1,56 @@
+interface BusinessStrategy{
+    //Strategy
+    //each strategy has its operate
+    public void operate();
+}
+
+class LowStrategy implements BusinessStrategy{
+    //Strategy1
+    public void operate(){
+        System.out.println("Use 20% off strategy");
+    }
+}
+
+class MidStrategy implements BusinessStrategy{
+    //Strategy2
+    public void operate(){
+        System.out.println("Use 25% off strategy");
+    }
+}
+
+class HighStrategy implements BusinessStrategy{
+    //Strategy3
+    public void operate(){
+        System.out.println("Use 50% off strategy");
+    }
+}
+
+class NikeShop{
+    //context
+    private BusinessStrategy strategy;
+
+    public NikeShop(BusinessStrategy _strategy){
+        //set strategy
+        this.strategy=_strategy;
+    }
+    //execute strategy
+    public void operate(){
+        this.strategy.operate();
+    }
+}
+
+public class Strategy{
+    public static void main(String[] args) {
+        //Have a shop
+        NikeShop nikeshop;
+
+        nikeshop = new NikeShop(new LowStrategy());
+        nikeshop.operate();
+
+        nikeshop = new NikeShop(new MidStrategy());
+        nikeshop.operate();
+
+        nikeshop = new NikeShop(new HighStrategy());
+        nikeshop.operate();
+    }
+}

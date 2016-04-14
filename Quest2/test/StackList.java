cf6de5d510d7fa141cec908984d0275783452032
zxzx1290 <zxl1l2l3@yahoo.com.tw>
2016-04-14 23:40:01 +0800

    Added files via upload

---
+++ Quest2/test/StackList.java
@@ -0,0 +1,44 @@
+class Element{}
+class Stack{
+	Delegate d = new Delegate();
+	public void push(){
+	}
+	public void pop(){
+	}
+	public void top(){
+	}
+
+	public void listAdd(){
+		d.add();
+	}
+
+	public void listRemove(){
+		d.remove();
+	}
+}
+
+class List{
+	public void add(Element e){
+		System.out.println("aaa");
+	}
+	public void remove(){
+	}
+}
+
+class Delegate{
+	public void add(){
+		list.add();
+	}
+	public void remove(){
+		list.remove();
+	}
+}
+
+public class StackList {
+	public static void main(String args[]){
+		Stack s = new Stack();
+		s.push();
+		s.listAdd();
+		s.remove();
+	}
+}

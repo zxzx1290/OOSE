40c2d7204e9e3c1d96ba9abf3402d51c26d06625
zxzx1290 <zxl1l2l3@yahoo.com.tw>
2016-05-07 17:49:21 +0800

    Create Observer.java

---
+++ Quest3/Observer.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+interface Subject {
+    public void registerObserver(ObserverInterface o);
+    public void removeObserver(ObserverInterface o);
+    public void notifyAllObserver();
+}
+
+interface ObserverInterface {
+    public void update(Subject s);
+}
+
+class HeadHunter implements Subject {
+    private ArrayList<ObserverInterface> userList = new ArrayList<ObserverInterface>();
+    private ArrayList<String> jobs = new ArrayList<String>();
+    public void registerObserver(ObserverInterface o) {
+        userList.add(o);
+    }
+    public void removeObserver(ObserverInterface o) {
+        userList.remove(o);
+    }
+    public void notifyAllObserver(){
+        for (ObserverInterface o : userList) {
+            o.update(this);
+        }
+    }
+    public void addJob(String j){
+        this.jobs.add(j);
+        notifyAllObserver();
+    }
+    public String toString() {
+        //ArrayList auto toString
+        return jobs.toString();
+    }
+}
+
+class JobSeeker implements ObserverInterface {
+    private String name;
+    public JobSeeker(String name) {
+        this.name = name;
+    }
+    public void update(Subject s) {
+        System.out.println(this.name+" got notified!");
+        System.out.println(s);
+    }
+}
+
+public class Observer {
+    public static void main(String[] args) {
+        HeadHunter h = new HeadHunter();
+        h.registerObserver(new JobSeeker("Mike"));
+        h.registerObserver(new JobSeeker("Chris"));
+        h.registerObserver(new JobSeeker("Jeff"));
+        h.addJob("Google Job");
+        h.addJob("Yahoo Job");
+    }
+}
